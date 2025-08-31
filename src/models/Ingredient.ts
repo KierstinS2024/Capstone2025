c5c5a4f19bad94f4331b065734a9a392cdc31090
KierstinS2024 <180041125+KierstinS2024@users.noreply.github.com>
2025-08-31 12:52:37 -0600

    Write Ingredient model

--- src/models/Ingredient.ts
+++ src/models/Ingredient.ts
@@ -1,1 +1,11 @@
 // src/models/Ingredient.ts
+
+import mongoose from 'mongoose';
+
+//This schema stores ingredients used in recipes or shopping lists
+const IngredientSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    unit: String, //grams, ml, etc
+    defaultQuantity: Number,
+    nutritionInfo: Object
+});
