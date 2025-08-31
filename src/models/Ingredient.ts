448dbf08c328261a72b9187c08141eb516fe64d4
KierstinS2024 <180041125+KierstinS2024@users.noreply.github.com>
2025-08-31 12:53:30 -0600

    export model at end of Ingredient.ts

--- src/models/Ingredient.ts
+++ src/models/Ingredient.ts
@@ -2,10 +2,12 @@
 
 import mongoose from 'mongoose';
 
-//This schema stores ingredients used in recipes or shopping lists
+// This schema stores ingredients used in recipes or shopping lists
 const IngredientSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    unit: String, //grams, ml, etc
-    defaultQuantity: Number,
-    nutritionInfo: Object
+  name: { type: String, required: true },
+  unit: String, // grams, ml, pcs
+  defaultQuantity: Number,
+  nutritionInfo: Object
 });
+
+export default mongoose.models.Ingredient || mongoose.model('Ingredient', IngredientSchema);
